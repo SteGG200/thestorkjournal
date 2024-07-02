2b1959c3750dea8765246a30253ec11aabcaec53
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-07-02 22:14:16 +0700

    Add key authorization for confirming article

--- backend/src/api/article/createArticle.ts
+++ backend/src/api/article/createArticle.ts
@@ -3,6 +3,7 @@ import { getUserInfoCookie } from "../../utils/cookieHandler";
 import { isAuthenticated } from "../../utils/authentication";
 import { createNewArticle } from "../../database/authentication/articleHandler";
 import transporter from "../../utils/emailHandler";
+import crypto from "crypto"
 
 export const createArticleRoute : FastifyPluginAsync = async (fastify, option) => {
 	fastify.get('/', (req, res) => {
@@ -24,13 +25,15 @@ export const createArticleRoute : FastifyPluginAsync = async (fastify, option) =
 
 		const authorInfo = getUserInfoCookie(req.cookies[fastify.config.cookieName.userInfo] as string)
 
-		const articleId = await createNewArticle(authorInfo.id, article)
+		const confirm_key = crypto.randomBytes(32).toString('base64')
+
+		const articleId = await createNewArticle(authorInfo.id, article, confirm_key)
 
 		transporter.sendMail({
 			from: process.env.EMAIL_ADMIN,
 			to: process.env.EMAIL_ADMIN,
       subject: "New Article Created",
-      text: `A new article has been created by ${authorInfo.name} with email ${authorInfo.email}.\n\nThe ID of the article is ${articleId}. Use it to publish the article as soon as you can.\n\nThank you!`
+      text: `A new article has been created by ${authorInfo.name} with email ${authorInfo.email}.\n\nThe ID of the article is ${articleId}. Using the key below to confirm the article as soon as you can.\n\nThis is the key: ${confirm_key}\n\nThank you!`
 		})
 
 		res.statusCode = 200
--- backend/src/database/authentication/articleHandler.ts
+++ backend/src/database/authentication/articleHandler.ts
@@ -1,8 +1,8 @@
 import sql from '../db'
 
-export const createNewArticle = async (userId : number, newArticle:  ArticleInfo) => {
-	const article = await sql<{id: string}[]>`INSERT INTO unpublished_articles (user_id, title, thumbnail, category, content) 
-	VALUES (${userId}, ${newArticle.title}, ${newArticle.thumbnail}, ${newArticle.category}, ${newArticle.content})
+export const createNewArticle = async (userId : number, newArticle:  ArticleInfo, key: string) => {
+	const article = await sql<{id: string}[]>`INSERT INTO unconfirmed_articles (user_id, title, thumbnail, category, content, key) 
+	VALUES (${userId}, ${newArticle.title}, ${newArticle.thumbnail}, ${newArticle.category}, ${newArticle.content}, ${key})
 	RETURNING id`
 
 	return article[0].id
