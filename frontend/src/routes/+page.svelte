3a94ebf083abd4b1572131f607577b41a74b1992
akari <pikachungaytho@gmail.com>
2024-07-04 14:42:45 +0700

    login and navbar

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import "../app.css";
+  import Navbar from '../lib/navbar.svelte';
+</script>
+
+<Navbar/>
+
+<div class="pt-20" id=""></div>
+
+{#each {length: 30} as _, i}
+    <li>nothing {i + 1}</li>
+{/each}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      theme: {
+        colors: {
+          mainred: '#db2424',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
