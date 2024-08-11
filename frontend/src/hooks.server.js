6eddd135d22feac26eaea0e3f2838062adadeb77
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-08-11 16:15:47 +0700

    Log the cookies

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -3,6 +3,7 @@ import { PUBLIC_SERVER_URL } from '$env/static/public';
 /**@type {import('@sveltejs/kit').HandleFetch} */
 export async function handleFetch({ event, request, fetch }){
 	const cookies = event.request.headers.get('cookie')
+	console.log(cookies)
 	if(request.url.startsWith(`${PUBLIC_SERVER_URL}/auth`) && cookies){
 		request.headers.set('cookie', cookies);
 	}
